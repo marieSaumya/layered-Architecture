3ec337a0323d7e4056985e60e0369f55c3cb5d9f
marieSaumya <mariesaumya@gmail.com>
2023-12-14 07:11:43 +0530

    tightly coupling

--- Tightly-coupling/src/main/java/org/example/Boy.java
+++ Tightly-coupling/src/main/java/org/example/Boy.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Boy {
+    public void chatWithGirl(){
+        Girl girl = new Girl();
+        girl.chat();
+    }
+
+    public static void main(String[] args) {
+        Boy boy = new Boy();
+        boy.chatWithGirl();
+    }
 }
--- Tightly-coupling/src/main/java/org/example/Girl.java
+++ Tightly-coupling/src/main/java/org/example/Girl.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class Girl {
+    public void chat(){
+        System.out.printf("chatting");
+    }
 }
